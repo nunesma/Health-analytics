9e8ee4962c26b7ba808b0023e349a7e740977e77
Marco Nunes <nunes.ma@ufs.br>
2018-10-11 23:55:15 -0300

    First update explore.R script

--- med_std/explore/explore.R
+++ med_std/explore/explore.R
@@ -2,3 +2,19 @@ setwd("C:/Users/nunes/AI/ml/Health-analytics/med_std/explore")
 
 banco = read.csv("def1.csv")
 
+
+
+table(banco$APROVADO); 
+table(banco$DISPENSADO); 
+table(banco$REP..FALTA); 
+table(banco$REPROVADO); 
+table(banco$TRANCADO); 
+table(banco$CANCELADO); 
+table(banco$EXCLUIDA); 
+table(banco$CUMPRIU); 
+table(banco$REPROVADO.POR.MÉDIA.E.POR.FALTAS); 
+table(banco$MATRICULADO); 
+table(banco$DESISTENCIA); 
+table(banco$INDEFERIDO)
+
+table(banco$ano_ingresso)
