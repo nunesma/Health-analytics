3acb0e013482b075376173e48ecd545ddd1e064f
Marco Nunes <nunes.ma@ufs.br>
2018-10-12 18:54:37 -0300

    Update R scrit for data analysis of medical students performance

--- med_std/explore/explore.R
+++ med_std/explore/explore.R
@@ -1,20 +1,58 @@
 setwd("C:/Users/nunes/AI/ml/Health-analytics/med_std/explore")
 
-banco = read.csv("def1.csv")
+# banco = read.csv("def1.csv")
+banco = read.csv("def.csv")
+
+library(FSA)
+
+names(banco)
+
+str(banco)
+
+# sexo
+banco$sex <- ifelse(banco$sexo == "F","Female","Male")
+banco$Sergipe <- ifelse(banco$uf_naturalidade == "SE","Yes","No")
+banco$quotas <- ifelse(banco$ano_ingresso >= 2010 & (banco$cota=="B"|banco$cota=="C"|banco$cota=="D1"|banco$cota=="D2"|
+                                                      banco$cota=="D3"|banco$cota=="D4"|banco$cota=="E"|
+                                                      banco$cota=="F"|banco$cota=="G"|banco$cota=="H"|
+                                                      banco$cota=="I"|banco$cota=="PD"),"Yes","No")
 
 
+table(banco$uf_naturalidade); table(banco$Sergipe)
 
-table(banco$APROVADO); 
-table(banco$DISPENSADO); 
-table(banco$REP..FALTA); 
-table(banco$REPROVADO); 
-table(banco$TRANCADO); 
-table(banco$CANCELADO); 
-table(banco$EXCLUIDA); 
-table(banco$CUMPRIU); 
-table(banco$REPROVADO.POR.MÉDIA.E.POR.FALTAS); 
-table(banco$MATRICULADO); 
-table(banco$DESISTENCIA); 
-table(banco$INDEFERIDO)
+table(banco$Sergipe, banco$ano_ingresso)
 
-table(banco$ano_ingresso)
+table(banco$cota); table(banco$quotas); table(banco$quotas, banco$ano_ingresso)
+
+table(banco$pais_naturalidade)
+table(banco$uf_naturalidade)
+table(banco$sexo)
+table(banco$ano_nascimento)
+table(banco$status)
+table(banco$ultima_movimentação)
+table(banco$cota)
+
+table(banco$pais_naturalidade, banco$ano_ingresso)
+table(banco$uf_naturalidade, banco$ano_ingresso)
+table(banco$sexo, banco$ano_ingresso)
+table(banco$ano_nascimento, banco$ano_ingresso)
+table(banco$status, banco$ano_ingresso)
+table(banco$ultima_movimentação, banco$ano_ingresso)
+table(banco$cota, banco$ano_ingresso)
+
+Summarize(banco$Idade_atual)
+Summarize(banco$Idade_no_ingresso)
+Summarize(banco$nota_med)
+Summarize(banco$n_periodo)
+
+Summarize(banco$Idade_atual ~ banco$ano_ingresso)
+Summarize(banco$Idade_no_ingresso ~ banco$ano_ingresso)
+Summarize(banco$nota_med ~ banco$ano_ingresso)
+Summarize(banco$n_periodo ~ banco$ano_ingresso)
+
+hist(banco$Idade_atual)
+hist(banco$Idade_no_ingresso)
+hist(banco$nota_med)
+hist(banco$n_periodo)
+
+
