b2ba1f6009142f15e1edc1027676d040c898fb72
Marco Nunes <nunes.ma@ufs.br>
2018-10-18 13:28:50 -0300

    Updating tools folder for health analysis

--- tools/adjust.R
+++ tools/adjust.R
@@ -1,11 +1,11 @@
-adjust <- function(y, t, w) {
+adjust <- function(data, pop, std_rates, dec = 2) {
 
-	#
-	data <-y
-	pop <- t
-	std_rates <-w
+	# arguments definition
+#	data = observed fequence
+#	pop = reference population
+#	std_rates: = weight based on world population
 
-	# start data.frame
+	# starting data.frame
 	result <- data.frame(matrix(nrow = 20, ncol = 4))
 	colnames(result) <- c('Year', 'Crude', 'Adjusted', 'SE')
 
@@ -17,9 +17,9 @@ adjust <- function(y, t, w) {
 	for (i in range) {
 
 		# formulas
-		adjusted <- round(sum(((data[,i]/pop[,i])*100000)*std_rates), 2)
-		crude <- round((sum(data[,i])/sum(pop[,i])*100000), 2)
-		se <- round(crude/(sqrt(sum(pop[,i]))), 4)
+		adjusted <- round(sum(((data[,i]/pop[,i])*100000)*std_rates), dec)
+		crude <- round((sum(data[,i])/sum(pop[,i])*100000), dec)
+		se <- round(crude/(sqrt(sum(pop[,i]))), dec)
 
 		# returns the result on console
 #		print(paste(' ', year, crude, adjusted, se, ' '))
@@ -34,4 +34,55 @@ adjust <- function(y, t, w) {
 	}
 
 	return(result)
+}
+
+
+crude <- function(data, pop, std_rates, dec = 2) {
+
+	# arguments definition
+#	data = observed fequence
+#	pop = reference population
+#	std_rates: = weight based on world population
+
+	# starting data.frame
+	result <- data.frame(matrix(nrow = 20, ncol = 19))
+	colnames(result) <- c("Year", "f0_9y", "f10_19y", "f20_29y", "f30_39y", "f40_49y", "f50_59y", "f60_69y", "f70_79y", "f80my", "SE0", "SE10", "SE20", "SE30", "SE40", "SE50", "SE60", "SE70", "SE80")
+
+
+	# prepare for loop
+	range <- seq(1, 20)
+	year <- 1998
+
+	# loop for compute adjusted population
+	for (i in range) {
+
+		# formulas
+		crude <- round(((data[,i] / pop[,i])*100000), dec)
+		se <- round(crude/(sqrt(pop[,i])), dec)
+
+		# returns the result on console
+#		print(paste(' ', year, crude, se, ' '))
+
+		# returns a data frame
+		result[i, 1] <- year
+		result[i, 2:10] <- crude
+		result[i, 11:19] <- se
+
+		year = year + 1
+	}
+
+	return(result)
+}
+
+
+oneHot <- function (vector){
+  variable <- as.character(unique(vector))
+
+  result <- data.frame()
+  for (i in (1 : length(variable))){
+    model <- as.numeric((as.character(vector)) == variable[i])
+    result[1:(length(vector)), i] <- model
+  }
+  colnames(result) <- c(paste("cid", (1:length(variable))))
+  return(result)
 }
