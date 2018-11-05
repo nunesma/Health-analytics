3cb9f891b482d82715155620cb428df3512edfba
Marco Nunes <nunes.ma@ufs.br>
2018-11-04 23:13:13 -0300

    Update time series code in explore.R script of med_std project

--- med_std/explore/explore.R
+++ med_std/explore/explore.R
@@ -1,5 +1,9 @@
 setwd("C:/Users/nunes/AI/ml/Health-analytics/med_std/explore")
 
+library(FSA)
+library(reshape)
+library(prais)
+
 # banco = read.csv("def1.csv")
 def = read.csv("def.csv")
 banco <- subset(def, status == "ATIVO" | status == "CONCLUÍDO" | status == "FORMANDO")
@@ -34,8 +38,7 @@ colnames(banco) <- c("registration", "name", "admission_age", "entry_year", "ent
 # "sex", "Sergipe", "naturalness", "nationality", "quotas"
 # "aproved", "reproved", "equivalence", "quit1", "quit2", "concluded"
 
-library(FSA)
-library(reshape)
+
 tapply(banco$aproved, banco$concluded, mean)
 Summarize(banco$aproved)
 hist(banco$aproved)
@@ -43,20 +46,50 @@ boxplot(banco$aproved)
 
 summary(banco)
 
+Summarize(banco$admission_age); Summarize(banco$n_periods); Summarize(banco$avg_note)
 
-Summarize(banco$admission_age)
-Summarize(banco$n_periods)
-Summarize(banco$avg_note)
-
-hist(banco$admission_age)
-hist(banco$n_periods)
-hist(banco$avg_note)
-
+hist(banco$admission_age); hist(banco$n_periods); hist(banco$avg_note)
 
 table(banco$sex)
 table(banco$entry_semester,banco$sex)
 sex_series <- data.frame(table(banco$entry_semester,banco$sex))
 sex_series <- cast(sex_series, Var1 ~ Var2, value = 'Freq')
-sex_series$Var1 <- as.numeric(sex_series$Var1)
-plot(sex_series$Female ~ sex_series$Var1, type = 'l', ylim = c(0,50))
+sex_series$sem <- as.numeric(as.character(sex_series$Var1))
+# plot(sex_series$Female ~ sex_series$Var1, type = 'l', ylim = c(0,50))
+plot(sex_series$female ~ sex_series$sem, type = 'l', ylim = c(0,1), lwd = 6)
+grid() # Draw gridlines
+f2 <- rep(1/2, 2)# average of current sample and 1 previous samples (red)
+f3 <- rep(1/3, 3)# average of current sample and 2 previous samples (green)
+f4 <- rep(1/4, 4)# average of current sample and 3 previous samples (blue)
+y_lag <- filter(sex_series$female, f2, sides=1)
+y_lag3 <- filter(sex_series$female, f3, sides=1)
+y_lag4 <- filter(sex_series$female, f4, sides=1)
+lines(sex_series$sem, y_lag, col="red") # calculating a moving average
+lines(sex_series$sem, y_lag3, col="green") # calculating a moving average
+lines(sex_series$sem, y_lag4, col="blue", lwd = 6) # calculating a moving average
+
 str(sex_series)
+table(sex_series$sem)
+
+## time series analysis
+
+# ploting
+sex_series$male <- sex_series$Male/(sex_series$Male + sex_series$Female)
+sex_series$female <- sex_series$Female/(sex_series$Male + sex_series$Female)
+sex_series$log_female <- log10(sex_series$female)
+
+# regression for tendence evaluation
+pw<- prais_winsten(sex_series$log_female ~ sex_series$sem)
+result <- summary(pw)
+
+estimate <- result$coefficients[2,1] # getting estimate from prais coefficients summary
+apc <- -1 + 10 ** estimate; apc # anual percent change (Taxa de incremento anual)
+t <- qt(1 - .025, nrow(sex_series)) # getting t-value for confidence interval
+se <- result$coefficients[2,2] # getting standard error from prais coefficients summary
+apc_minCI <- -1 + 10 ** (estimate - (t*se)); apc_minCI # anual percent change (Taxa de incremento anual)
+apc_maxCI <- -1 + 10 ** (estimate + (t*se)); apc_maxCI # anual percent change (Taxa de incremento anual)
+
+# getAnywhere(prais_winsten())
+
+
+
