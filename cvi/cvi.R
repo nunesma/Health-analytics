58a5ea3d5b4a5f2bf79bfda0105c1deccae88b7c
Marco Nunes <nunes.ma@ufs.br>
2019-01-10 08:50:28 -0300

    Update cvi.R Venous Insufficiency project script

--- cvi/cvi.R
+++ cvi/cvi.R
@@ -2,6 +2,7 @@
 setwd("C:/Users/nunes/AI/ml/Health-analytics/cvi")
 
 library(FSA)
+library(ggplot2)
 
 vein <- read.csv("vein.csv", sep = ";")
 str(vein)
@@ -51,10 +52,47 @@ wilcox.test(vein$abd_circum ~ vein$ceapc)
 kruskal.test(vein$abd_circum, vein$ceap)
 dunnTest(vein$abd_circum, vein$ceap)
 
-# To compare the CEAP classification with the venous diameter at the saphenous femoral junction and at the thigh
+table(vein$ceap, vein$bmic)
+chisq.test(table(vein$ceap, vein$bmic))
+fisher.test(table(vein$ceap, vein$bmic))
+barplot(table(vein$bmic, vein$ceap))
+table(vein$ceapc, vein$bmic)
+chisq.test(table(vein$ceapc, vein$bmic))
 
+# To compare the CEAP classification with the venous diameter at the saphenous femoral junction and at the thigh
+tapply(vein$fsj, vein$ceap, Summarize)
+tapply(vein$fsj, vein$ceap, Summarize)
+tapply(vein$sv, vein$ceapc, Summarize)
+tapply(vein$sv, vein$ceapc, Summarize)
+
+wilcox.test(vein$fsj ~ vein$ceapc)
+kruskal.test(vein$fsj, vein$ceap)
+dunnTest(vein$fsj, vein$ceap)
+
+wilcox.test(vein$sv ~ vein$ceapc)
+kruskal.test(vein$sv, vein$ceap)
+dunnTest(vein$sv, vein$ceap)
 
 # To compare the body mass index with the venous diameter at the femoral saphenous junction and at the thigh
 
+cor(vein$bmi, vein$fsj, method = "spearman")
+cor.test(vein$bmi, vein$fsj, method = "spearman")
+
+cor(vein$bmi, vein$sv, method = "spearman")
+cor.test(vein$bmi, vein$sv, method = "spearman")
+
+wilcox.test(vein$fsj ~ vein$bmic)
+wilcox.test(vein$sv ~ vein$bmic)
 
 
+plot(vein$fsj ~ vein$bmi)
+
+plot(vein$fsj ~ vein$bmi, pch=21, bg=c("red","green3")[unclass(vein$ceapc)])
+
+# ggplot(aes(vein$fsj, vein$bmi)) 
+# + geom_point(position = position_dodge(width = 0.4))
+# , color = bmic
+
+plot(iris$Petal.Length, iris$Petal.Width, pch=21, 
+     bg=c("red","green3","blue")[unclass(iris$Species)], 
+     main="Edgar Anderson's Iris Data")
